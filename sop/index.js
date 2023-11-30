895d3e0a165a23dd256e76042643e3a99b0bc4a0
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-11-30 13:36:26 +0200

    ф

--- sop/index.js
+++ sop/index.js
@@ -1,28 +1,34 @@
-const counterElem = document.querySelector('.counter');
-const counterValueElem = document.querySelector('.counter__value');
+const counterElem = document.querySelectorAll('.counter__button');
+const counterValueElem = document.querySelectorAll('.counter__value');
 
-function onCounterChange(elem) {
-    const isButton = elem.target.classList.contains('counter__button');
+function onCounterChange(elem,index) {
+    
+    const action = elem.dataset.action;
+    console.log(elem.dataset.action);
+    const counterValue = elem.parentElement.querySelector('.counter__value');
 
-    if (!isButton) {
-        return;
-    }
-
-    const action = elem.target.dataset.action;
-
-    const oldValue = Number(counterValueElem.textContent);
+    const oldValue = Number(counterValue.textContent);
 
     const newValue = action === 'decrease'
         ? oldValue - 1
         : oldValue + 1;
+        let counterValueArray =[];
+        counterValue.textContent = newValue;
+        counterValueElem.forEach((elem, index)=>{
+            counterValueArray.push(elem.textContent);
+            
+        });
+    localStorage.setItem('counterValue', JSON.stringify(counterValueArray));
 
-    localStorage.setItem('counterValue', newValue);
-
-    counterValueElem.textContent = newValue;
+    
 };
 
-counterElem.addEventListener('click', onCounterChange);
-
+//counterElem.addEventListener('click', onCounterChange);
+counterElem.forEach((elem, index) => {
+   elem.addEventListener('click',function(){
+    onCounterChange(elem,index);
+});
+  });
 function onStorageChange(elem) {
     counterValueElem.textContent = elem.newValue;
 };
@@ -30,7 +36,13 @@ function onStorageChange(elem) {
 window.addEventListener('storage', onStorageChange);
 
 function onDocumentLoaded() {
-    counterValueElem.textContent = localStorage.getItem('counterValue') || 0;
+    counterValueElem.forEach((counterValue, index) => {
+        
+        let stringifyData = JSON.parse(localStorage.getItem('counterValue'));
+        let array = Object.keys(stringifyData).map((key) => [key, stringifyData[key]]);
+        counterValue.textContent = stringifyData.at(index);
+      });
+  
 };
 
 document.addEventListener('DOMContentLoaded', onDocumentLoaded);
